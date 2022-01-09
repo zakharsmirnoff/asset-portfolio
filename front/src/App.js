7890cfcc435f544d702f8997d084b3a9bbc77f73
Zakhar Smirnoff <zakharsmirnoff@gmail.com>
2022-01-09 15:03:09 +0300

    creating a input component

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,30 @@
-import { Grommet } from 'grommet'
-import { Routes, Route } from 'react-router-dom'
-import NavBar from '../src/components/navigation.component.js'
-import Charts from './components/charts.components.js'
-import Holdings from './components/holdings.components.js'
-import Trades from './components/trades.component.js'
+import { Grommet } from "grommet";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "../src/components/navigation.component.js";
+import Charts from "./components/charts.components.js";
+import Holdings from "./components/holdings.components.js";
+import Trades from "./components/trades.component.js";
+import NewHolding from "./components/new.component.js";
 
 const theme = {
   global: {
     font: {
-      family: 'Roboto',
-      size: '18px',
-      height: '20px',
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
     },
   },
 };
-
 
 function App() {
   return (
     <Grommet theme={theme}>
       <Routes>
-        <Route exact path='/' element={<NavBar />} />
-        <Route path='/charts' element={<Charts />} />
-        <Route path='/holdings' element={<Holdings />} />
-        <Route path='/trades' element={<Trades />} />
+        <Route exact path="/" element={<NavBar />} />
+        <Route path="/charts" element={<Charts />} />
+        <Route path="/holdings" element={<Holdings />} />
+        <Route path="/trades" element={<Trades />} />
+        <Route path="/new" element={<NewHolding />} />
       </Routes>
     </Grommet>
   );
--- front/src/components/new.component.js
+++ front/src/components/new.component.js
@@ -1,14 +1,76 @@
 import React, { Component } from "react";
 import NavBar from "./navigation.component";
-import { Box, Form } from "grommet";
+import { Box, Form, Button, FormField, TextInput } from "grommet";
 
 class NewHolding extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      amount: "",
+      totalCost: "",
+      averagePrice: "",
+      amountSold: "",
+      totalValueSold: "",
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  onChangeHandler(event) {
+    let value = event.target.value;
+    this.setState({ [event.target.name]: value });
+  }
+
   render() {
     return (
       <div>
         <NavBar />
         <Box align="center" pad="xlarge">
-          <Form></Form>
+          <Form
+            value={this.state.value}
+            onChange={this.onChangeHandler}
+            onSubmit={({ value }) => {}}
+            >
+            <FormField name="name" htmlFor="text-input-id" label="Name">
+              <TextInput id="text-input-id" name="name" />
+            </FormField>
+            <FormField name="amount" htmlFor="text-input-id" label="Amount">
+              <TextInput id="text-input-id" name="amount" />
+            </FormField>
+            <FormField
+              name="totalCost"
+              htmlFor="text-input-id"
+              label="Total Cost"
+            >
+              <TextInput id="text-input-id" name="totalCost" />
+            </FormField>
+            <FormField
+              name="averagePrice"
+              htmlFor="text-input-id"
+              label="Average Price"
+            >
+              <TextInput id="text-input-id" name="averagePrice" />
+            </FormField>
+            <FormField
+              name="amountSold"
+              htmlFor="text-input-id"
+              label="Amount Sold"
+            >
+              <TextInput id="text-input-id" name="amountSold" />
+            </FormField>
+            <FormField
+              name="totalValueSold"
+              htmlFor="text-input-id"
+              label="Total Value Sold"
+            >
+              <TextInput id="text-input-id" name="totalValueSold" />
+            </FormField>
+            <Box direction="row" gap="medium">
+              <Button type="submit" primary label="Submit" />
+              <Button type="reset" label="Reset" />
+            </Box>
+          </Form>
         </Box>
       </div>
     );
